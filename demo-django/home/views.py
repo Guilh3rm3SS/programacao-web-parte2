8541df3c8510803c9b09f26c39cc560b4683b0cb
Guilherme <silvadesouzaguilherme24@gmail.com>
2026-06-14 12:50:49 -0300

    passo 12

--- demo-django/home/views.py
+++ demo-django/home/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from .models import Mensagem
 # Create your views here.
+
+
+def index(request):
+    mensagens = Mensagem.objects.all()
+    return render(request, "home/index.html", {"mensagens": mensagens})
