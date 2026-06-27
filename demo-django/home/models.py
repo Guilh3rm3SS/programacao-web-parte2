71354a72c2e1aed66e9db3a2cc383bbff3d582aa
Guilherme <silvadesouzaguilherme24@gmail.com>
2026-06-27 19:46:00 -0300

    até o 4

--- demo-django/home/models.py
+++ demo-django/home/models.py
@@ -3,6 +3,7 @@ from django.db import models
 # Create your models here.
 class Mensagem(models.Model):
     titulo = models.CharField(max_length=120)
+    autor = models.CharField(max_length=80, default="Anônimo")
     conteudo = models.TextField()
     criada_em = models.DateTimeField(auto_now_add=True)
 
