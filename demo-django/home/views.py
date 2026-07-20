0ceb5cde633de25e418e5fdb3cc1aedd344d467c
Guilherme <silvadesouzaguilherme24@gmail.com>
2026-07-20 19:55:52 -0300

    passos 8 e 9, validação e confirmação visual

--- demo-django/home/views.py
+++ demo-django/home/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.utils.text import slugify
 from .models import Tag, Mensagem
 from .forms import MensagemForm
+from django.contrib import messages
 # Create your views here.
 
 
@@ -16,7 +17,7 @@ def nova_mensagem(request):
     if request.method == "POST":
         form = MensagemForm(request.POST)
         if form.is_valid():
-            mensagem = form.save() #salva título, conteúdo, autor e categoria no banco.
+            mensagem = form.save()
 
             #transforma o texto em objetos tag 
             tags_texto = form.cleaned_data["tags"]
@@ -26,6 +27,7 @@ def nova_mensagem(request):
                     tag, _ = Tag.objects.get_or_create(nome=nome)
                     mensagem.tags.add(tag)
 
+            messages.success(request, "Mensagem publicada com sucesso!")
             return redirect("index")
     else:
         form = MensagemForm()
