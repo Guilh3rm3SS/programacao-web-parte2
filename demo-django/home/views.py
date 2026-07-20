72378173a22410915055d2e65c79eb388542d3f8
Guilherme <silvadesouzaguilherme24@gmail.com>
2026-07-20 20:39:59 -0300

    paso 1: função editar mensagem no view

--- demo-django/home/views.py
+++ demo-django/home/views.py
@@ -1,10 +1,17 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.utils.text import slugify
 from .models import Tag, Mensagem
 from .forms import MensagemForm
 from django.contrib import messages
 # Create your views here.
 
+def _aplicar_tags(mensagem, tags_texto):
+    mensagem.tags.clear()
+    for pedaco in tags_texto.split(","):
+        nome = slugify(pedaco)
+        if nome:
+            tag, _ = Tag.objects.get_or_create(nome=nome)
+            mensagem.tags.add(tag)
 
 def index(request):
     mensagens = Mensagem.objects.all()
@@ -33,3 +40,20 @@ def nova_mensagem(request):
         form = MensagemForm()
     
     return render(request, "home/nova.html", {"form": form})
+
+def editar_mensagem(request, id):
+    mensagem = get_object_or_404(Mensagem, id=id)
+
+    if request.method == "POST":
+        form = MensagemForm(request.POST, instance=mensagem)
+        if form.is_valid():
+            mensagem = form.save()
+            _aplicar_tags(mensagem, form.cleaned_data["tags"])
+            messages.success(request, "Mensagem atualizada com sucesso!")
+            return redirect("index")
+    else:
+        tags_atuais = ", ".join(tag.nome for tag in mensagem.tags.all())
+        form = MensagemForm(instance=mensagem, initial={"tags": tags_atuais})
+
+    return render(request, "home/editar.html", {"form": form, "mensagem": mensagem})
+    
