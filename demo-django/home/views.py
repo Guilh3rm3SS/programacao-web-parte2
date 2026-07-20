858eec6b4490d54c84c02442c34a04179ff1c906
Guilherme <silvadesouzaguilherme24@gmail.com>
2026-07-20 19:30:58 -0300

    passo 4, views.py atualizada

--- demo-django/home/views.py
+++ demo-django/home/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render
+from models import Tag
+from django.shortcuts import render, redirect
+from django.utils.text import slugify
+from forms import MensagemForm
 from .models import Mensagem
 # Create your views here.
 
@@ -9,3 +12,23 @@ def index(request):
 
 def sobre(request):
     return render(request, "home/sobre.html")
+
+def nova_mensagem(request):
+    if request.method == "POST":
+        form = MensagemForm(request.POST)
+        if form.is_valid():
+            mensagem = form.save() #salva título, conteúdo, autor e categoria no banco.
+
+            #transforma o texto em objetos tag 
+            tags_texto = form.cleaned_data["tags"]
+            for pedaco in tags_texto.split(","):
+                nome = slugify(pedaco)
+                if nome:
+                    tag, _ = Tag.objects.get_or_create(nome=nome)
+                    mensagem.tags.add(tag)
+
+            return redirect("index")
+    else:
+        form = MensagemForm()
+    
+    return render(request, "home/nova.html", {"form": form})
